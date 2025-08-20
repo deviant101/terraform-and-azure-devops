32f2d44015631264def8d94d870c4a202060f92b
deviant101 <115878714+deviant101@users.noreply.github.com>
2025-08-20 21:46:47 +0500

    added: modules

---
+++ modules/vnet/variables.tf
@@ -0,0 +1,36 @@
+variable "resource_group_name" {
+  description = "Name of the resource group for the virtual network"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address prefixes for the subnet"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
